9a048c86ff0054658c48f5d545d870c4b86487e9
LuongPham0801 <phamquangluong811998@gmail.com>
2024-10-08 21:21:47 +0700

    add file Instructor

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+// import ManageCourse from "../pages/InstructorPage/ManageCourse";
 import { DefaultLayout } from "../layout";
 import { RoleEnum, RouteConfig } from "../model/RouteConfig";
 
@@ -7,7 +8,7 @@ import { RoleEnum, RouteConfig } from "../model/RouteConfig";
 const publicRoute: RouteConfig[] = [
   { path: "/", component: Home, layout: DefaultLayout, role: RoleEnum.Guest },
   { path: "/login", component: Login, layout: null, role: RoleEnum.Guest },
-
+  // { path: "/instructorpage", component: ManageCourse, layout: InstructorLayout, role: RoleEnum.User},
 ];
 
 const privateRoute: RouteConfig[] = [];
