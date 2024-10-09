7fa7b343501b4be78abd601bcfa1e05513c4c820
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2024-10-09 08:21:54 +0700

    homepage

--- src/components/GuestComponents/GuestSidebar/index.tsx
+++ src/components/GuestComponents/GuestSidebar/index.tsx
@@ -1,8 +1,36 @@
-export default function GuestSideBar() {
-    return (
-        <>
-            <h1>THIS IS GUEST SIDE BAR</h1>
-        </>
+import { Carousel } from "antd";
+import assets from "../../../assets/assets";  
 
-    );
+const imgStyle: React.CSSProperties = {
+  width: '100%',
+  height: 'auto', // Auto height for responsiveness
+  objectFit: 'cover',
+  borderRadius: "30px",
+};
+
+const carouselContainer: React.CSSProperties = {
+  width: '85%', 
+  margin: '18px auto',
+  marginTop: '100px',
+};
+
+export default function GuestSideBar() {
+  return (
+    <div style={carouselContainer}>
+      <Carousel arrows infinite={false} autoplay>
+        <div>
+          <img style={imgStyle} src={assets.petfood} alt="Pet Food" />
+        </div>
+        {/* <div>
+          <img style={imgStyle} src={assets.petfood} alt="Pet Food" />
+        </div>
+        <div>
+          <img style={imgStyle} src={assets.petfood} alt="Pet Food" />
+        </div>
+        <div>
+          <img style={imgStyle} src={assets.petfood} alt="Pet Food" />
+        </div> */}
+      </Carousel>
+    </div>
+  );
 }
