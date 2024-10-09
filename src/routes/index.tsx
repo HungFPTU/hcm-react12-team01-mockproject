c8098ae0bf17d886ac1da91659a37f7d3044cf49
DevFeReactjs <quankontum55555@gmail.com>
2024-10-09 08:25:36 +0700

    Login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import Home from "../pages/Home";
-import Login from "../pages/Login";
+import Login from "../pages/Login/Login";
 import { DefaultLayout } from "../layout";
 import { RoleEnum, RouteConfig } from "../model/RouteConfig";
 
