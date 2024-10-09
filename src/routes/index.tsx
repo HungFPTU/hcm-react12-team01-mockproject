d110ca9132f11453523b03e56ddc956a7d3e736c
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2024-10-09 11:20:42 +0700

    sider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,16 @@
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Student from "../pages/DashboardStudent";
 import { DefaultLayout } from "../layout";
+import { StudentLayout } from "../layout";
 import { RoleEnum, RouteConfig } from "../model/RouteConfig";
 
 
 const publicRoute: RouteConfig[] = [
   { path: "/", component: Home, layout: DefaultLayout, role: RoleEnum.Guest },
   { path: "/login", component: Login, layout: null, role: RoleEnum.Guest },
-
+  { path: "/dashboard-student",component: Student,layout: StudentLayout, role: RoleEnum.User },
 ];
-
 const privateRoute: RouteConfig[] = [];
 
 export { publicRoute, privateRoute };
