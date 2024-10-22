f7e5546dca416ae25f994e65c174bcf0407183c8
LuongPham0801 <phamquangluong811998@gmail.com>
2024-10-22 20:42:38 +0700

    add filter for purchase

--- src/components/InstructorComponents/SalesHistory/index.tsx
+++ src/components/InstructorComponents/SalesHistory/index.tsx
@@ -1,14 +1,18 @@
 import TableSales from './TableSales';
 import SearchSales from './SearchSales';  
 import ButtonSales from './ButtonSales';
+import PurchaseStatusSelect from './FilterPurchase';
 const SalesHistory = () => {
   return (
     <div className="sales-history-container">
       <div className='mt-4'>
         <ButtonSales/>
-      </div>
+      </div>  
       <div className='mt-4'>
         <SearchSales onSearch={() => {}}/>
+      </div>
+      <div>
+        <PurchaseStatusSelect onChange={() => {}}/>
       </div>
       <div className='mt-4'>
         <TableSales/>
