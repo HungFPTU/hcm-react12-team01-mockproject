8e45262d171aadc1cf84b2b0f12f0343caf04a68
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2024-10-26 19:51:16 +0700

    Regenerate tsconfig.app.tsbuildinfo and fix casing issues

--- src/components/Demo/Business.tsx
+++ src/components/Demo/Business.tsx
@@ -37,7 +37,7 @@ const courses: Course[] = [
   },
   {
     id: 3,
-    title: "React for Beginners",
+    title: "React Beginners",
     image: assets.business1,
     price: 450000,
     rating: 5,
