f579fe14d9c6b17c9ed32a5f446a5e928680aaa5
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2024-10-28 20:06:11 +0700

    fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { Layout, Menu, Dropdown, Button, Avatar, Badge } from "antd";
 import { ShoppingCartOutlined, UserOutlined, MenuOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
