2a9aef948aa5e5b0ed325462b20ccb7a518d6a00
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2024-10-30 16:31:25 +0700

    fix verify

--- src/const/constCommon.tsx
+++ src/const/constCommon.tsx
@@ -4,6 +4,8 @@ export const DefaultLayout = lazy(() => import("../layout/DefaultLayout"));
 export const HomePage = lazy(() => import("../pages/Home"));
 export const CourseDetailPage = lazy(() => import("../pages/CourseDetail/CourseDetail"));
 export const AllCoursesPage = lazy(() => import("../pages/AllCourses"));
+export const VerifyEmail = lazy(() => import("../pages/Verifycaion"));
 export const homeRoute = { index: true, element: <HomePage /> };
 export const coureseDetail = { path: "/course/:id",element: <CourseDetailPage/> };
 export const allCourses = { path:"/all",element: <AllCoursesPage/> };
+export const verifyEmail = { path: "/verify-email/:token",element:<VerifyEmail/>}
