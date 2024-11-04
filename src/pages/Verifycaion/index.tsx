ca8242ca5b6c67e8189d2b010df5effa32629f8b
Luong <phamquangluong811998@gmail.com>
2024-11-04 12:54:56 +0700

    resend token via mail

--- src/pages/Verifycaion/index.tsx
+++ src/pages/Verifycaion/index.tsx
@@ -1,42 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 import { AuthService } from "../../services/authService/AuthService";
 
 const VerifyEmail: React.FC = () => {
   const navigate = useNavigate();
+  const hasMounted = useRef(false); // Khởi tạo ref để theo dõi trạng thái
 
   useEffect(() => {
+    if (hasMounted.current) return; // Trả về nếu đã mount
+    hasMounted.current = true; // Đánh dấu là đã mount
+
     const verifyToken = async () => {
       const token = window.location.pathname.split("/").pop();
       if (token) {
         try {
           const res = await AuthService.verifyToken(token);
-          console.log(res);
-          toast.success("Email is verified!");
-          navigate("/login");
+          if (res) {
+            navigate("/login");
+          } else {
+            navigate("/resend-verification");
+          }
         } catch (error) {
           console.log(error);
-          // Kiểm tra nội dung phản hồi từ API
-          if (error instanceof Error && "response" in error) {
-            const errorMessage = (error as any).response.data.message; // Giả sử có một trường message trong phản hồi
-            if (errorMessage === "token is not valid") { // Kiểm tra thông điệp lỗi
-              toast.error("Token is not valid. Please request a new verification link.");
-              navigate("/resend-verification"); // Điều hướng đến trang resend token
-            } else {
-              toast.error("Failed to verify email!");
-              navigate("/login");
-            }
-          } else {
-            toast.error("Failed to verify email!");
-            navigate("/login");
-          }
         }
       }
     };
 
     verifyToken();
-  }, [navigate]);
+  }, []);
 
   return (
     <div>
