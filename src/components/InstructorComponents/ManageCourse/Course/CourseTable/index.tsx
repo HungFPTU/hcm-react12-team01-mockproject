76ea3c1b7b37ca30d92806ccddc9eea05cd63a31
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2024-11-06 15:51:53 +0700

    test

--- src/components/InstructorComponents/ManageCourse/Course/CourseTable/index.tsx
+++ src/components/InstructorComponents/ManageCourse/Course/CourseTable/index.tsx
@@ -1,38 +1,73 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Table, Button, Switch } from "antd";
-import { Course, CourseStatusEnum } from "../../../../../model/Course";
+import { CourseStatusEnum } from "../../../../../model/Course";
 import { useNavigate } from "react-router-dom";
-import { CourseService } from "../../../../../services/CourseService/CourseService";
-
+import { CourseService } from "../../../../../services/CourseService/course.service";
+import { GetCourseRequest } from "../../../../../model/admin/request/Course.request";
+import { EyeOutlined } from "@ant-design/icons";
+import { GetCourseResponsePageData } from "../../../../../model/admin/response/Course.response";
 const CourseTable = () => {
   const navigate = useNavigate();
-  const [coursesData, setCoursesData] = useState<Course[]>([]);
-  const [searchTerm] = useState<string>("");
+  const [coursesData, setCoursesData] = useState<GetCourseResponsePageData[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isDataEmpty, setIsDataEmpty] = useState(false);
+  const hasMounted = useRef(false);
+
+  const fetchCourse = async (params: GetCourseRequest) => {
+    try {
+      const response = await CourseService.getCourse(params);
+      return response.data;
+      
+    } catch (error) {
+      console.error("Failed to fetch courses:", error);
+    }
+  };
 
   useEffect(() => {
-    // Gọi API để lấy danh sách khóa học
-    CourseService.getCourses()
-      .then((response) => {
-        if (response && response.data && response.data.data) {
-          setCoursesData(response.data.data.pageData); // Cập nhật dữ liệu khóa học từ API
+    if (hasMounted.current) return;
+    hasMounted.current = true;
+
+      const fetchCoursesData = async () => {
+        try {
+              const searchCondition = {
+                keyword: searchQuery,
+                category_id: "",
+                status: undefined,
+                is_delete: false,
+              };
+        
+              const response = await fetchCourse({
+                searchCondition,
+                pageInfo: {
+                  pageNum: 1,
+                  pageSize: 10,
+                },
+              });
+              if (response && response.success) {
+                const data = response.data.pageData;
+                setCoursesData(data);
+                setIsDataEmpty(data.length === 0); // Check if data is empty
+              }
+        }catch (error) {
+          console.error("Failed to fetch categories:", error);
         }
-      })
-      .catch((error) => {
-        console.error("Error fetching courses:", error); // Log lỗi khi gọi API
-      });
-  }, []);
+      }
+
+
+      fetchCoursesData();
+  }, [searchQuery]);
 
   const filteredCourses = coursesData.filter((course) =>
-    course.name.toLowerCase().includes(searchTerm.toLowerCase())
+    course.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   const handleViewDetails = (courseId: string) => {
-    navigate(`/instructor/${courseId}`);
+    navigate(`/instructor/manage-course/view-detail-course/${courseId}`);
   };
 
   const onChangeStatus = (id: string, status: CourseStatusEnum) => {
     console.log(`Changing status for course ${id} to ${status}`);
-    // Xử lý logic thay đổi trạng thái ở đây
+    // Implement status update logic here
   };
 
   const columns = [
@@ -45,11 +80,6 @@ const CourseTable = () => {
       title: "Category",
       dataIndex: "category_name",
       key: "category_name",
-    },
-    {
-      title: "Instructor",
-      dataIndex: "user_name",
-      key: "user_name",
     },
     {
       title: "Status",
@@ -77,12 +107,12 @@ const CourseTable = () => {
     {
       title: "Change Status",
       key: "changeStatus",
-      render: (_: unknown, record: Course) => (
+      render: (_: unknown, record: GetCourseResponsePageData) => (
         <Switch
           checked={record.status === CourseStatusEnum.Active}
           onChange={(checked) =>
             onChangeStatus(
-              record.id,
+              record._id,
               checked ? CourseStatusEnum.Active : CourseStatusEnum.Inactive
             )
           }
@@ -93,29 +123,29 @@ const CourseTable = () => {
     {
       title: "Action",
       key: "actions",
-      render: (_: unknown, record: Course) => (
+      render: (_: unknown, record: GetCourseResponsePageData) => (
         <Button
-          onClick={() => handleViewDetails(record.id)}
+          onClick={() => handleViewDetails(record._id)}
           className="bg-blue-500 hover:bg-blue-600 text-white"
         >
-          View Detail
+          <EyeOutlined />
         </Button>
       ),
     },
   ];
 
   return (
-    <Table<Course>
+    <Table<GetCourseResponsePageData>
       columns={columns}
       dataSource={filteredCourses}
-      rowKey="id"
+      rowKey="_id"
       className="w-full shadow-md rounded-lg overflow-hidden"
       pagination={{
         pageSize: 10,
         showSizeChanger: true,
         showQuickJumper: true,
         showTotal: (total, range) =>
-          `${range[0]}-${range[1]} của ${total} khóa học`,
+          `${range[0]}-${range[1]} of ${total} courses`,
       }}
     />
   );
